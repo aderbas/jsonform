4f9a95d4b2c5a961ccd74ef23825f6c8ffb221b3
Aderbal Nunes <aderbalnunes@gmail.com>
2021-01-06 13:52:15 -0300

    Add InputSwitch to export

--- src/elements/index.js
+++ src/elements/index.js
@@ -1,3 +1,4 @@
 export {default as TextInput} from './input/TextInput';
 export {default as InputSelect} from './input/InputSelect';
+export {default as InputSwitch} from './input/InputSwitch';
 export {default as Separator} from './layout/Separator';
