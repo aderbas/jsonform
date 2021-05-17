05cbc1733cef9875203a346594ff9863c7fd1d89
Aderbal Nunes <aderbalnunes@gmail.com>
2021-05-17 10:28:45 -0300

    Update index.js

--- src/elements/index.js
+++ src/elements/index.js
@@ -2,4 +2,5 @@ export {default as TextInput} from './input/TextInput';
 export {default as InputSelect} from './input/InputSelect';
 export {default as InputSwitch} from './input/InputSwitch';
 export {default as Separator} from './layout/Separator';
+export {default as Info} from './layout/Info.js';
 export {default as UploadBox} from './box/UploadBox';
