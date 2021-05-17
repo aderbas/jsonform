2620bd2ecae9bbf10002550521ebed3529167edd
Aderbal Nunes <aderbalnunes@gmail.com>
2021-05-17 10:27:49 -0300

    Add Info element

---
+++ src/elements/layout/Info.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Box} from '@material-ui/core';
+import PropTypes from 'prop-types';
+
+const Info = ({...props}) => {
+  const {icon,text,color} = props;
+
+  return (
+    <Box pb={1} pt={1} display="flex" color={color}>
+      <Box>{icon}</Box>
+      <Box flexGrow={1}>{text}</Box>
+    </Box>
+  )
+}
+
+Info.propTypes = {
+  icon: PropTypes.object,
+  color: PropTypes.string,
+  text: PropTypes.string.isRequired
+}
+
+Info.defaultProps = {
+  color: 'text.secondary'
+}
+
+export default Info;
